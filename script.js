bb6be451820c5a26a95c8ecd44d6ba295f66469f
Muhammad Basim <muhammadbasim27811@gmail.com>
2026-09-07 22:07:49 +0500

    subtract-calc

---
+++ script.js
@@ -0,0 +1,4 @@
+let num1 = parseInt(prompt("Enter the first number:"));
+let num2 = parseInt(prompt("Enter the second number:"));
+let result = num1 - num2;
+alert("The result of subtraction is: " + result);
